9792fdee0faa6b4ff1cfa5bf90ddec91b728da96
bbushraahmedd <bushimed98@gmail.com>
2022-11-14 13:50:45 -0600

    button eventlisteners working in JS :)

--- js/app.js
+++ js/app.js
@@ -8,22 +8,39 @@
 //     window.location.replace(enterNameUrl);
 // }
 
+// Cached Variables for Buttons
+const foodButtonEl = document.querySelector('#food')
+const sleepButtonEl = document.querySelector('#sleep')
+const playButtonEl = document.querySelector('#play')
 
+// Event Listeners for Buttons
+
+foodButtonEl.addEventListener('click', e => {
+    console.log(e, 'buttons working yoo!')
+})
+
+sleepButtonEl.addEventListener('click', e => {
+    console.log(e, 'buttons working woohoooo!')
+})
+
+playButtonEl.addEventListener('click', e => {
+    console.log(e, 'buttons working woohooo!')
+})
 
 
 
 // Game Page
-let foodLevel = 0
-// A sleep variable: let sleepLevel = 0
-// A play variable: let playLevel = 0
+// let foodLevel = 100
+// A sleep variable: let sleepLevel = 100
+// A play variable: let playLevel = 100
 
 
 // I want to update the score in the DOM every time I hit the button corresponding to the variable
 
-function changeFoodLevel(newFoodLevel) {
-    foodLevel = newFoodLevel
-    return foodLevel
-}
+// function changeFoodLevel(newFoodLevel) {
+//     foodLevel = newFoodLevel
+//     return foodLevel
+// }
 
-changeFoodLevel(56)
-console.log(foodLevel, "new food level")
+// changeFoodLevel(56)
+// console.log(foodLevel, "new food level")
--- js/pseudo.js
+++ js/pseudo.js
@@ -1,16 +1,50 @@
  
+// How I want this to run
+// The base levels of the animal will start at 100 always
+
+
+// Clicking the button would feed the animal and keep the base level of each need to 100
+
+// I want to be able to change the level of the needs when I click the button
+
+
+
 
 //  Variables in the game
-// A food variable: let foodLevel = 0
-// A sleep variable: let sleepLevel = 0
-// A play variable: let playLevel = 0
+// A food variable: let foodLevel = 100
+// A sleep variable: let sleepLevel = 100
+// A play variable: let playLevel = 100
+
+
+// Cached Variables for Buttons
+const foodButtonEl = document.querySelector('#food')
+const sleepButtonEl = document.querySelector('#sleep')
+const playButtonEl = document.querySelector('#play')
+
+// Event Listeners for Buttons
+
+foodButtonEl.addEventListener('click', e => {
+    console.log(e, 'buttons working yoo!')
+})
+
+sleepButtonEl.addEventListener('click', e => {
+    console.log(e, 'buttons working woohoooo!')
+})
+
+playButtonEl.addEventListener('click', e => {
+    console.log(e, 'buttons working woohooo!')
+})
+
+
+
+
 
 
 const foodScoreEl = document.querySelector('#foodScore')
 
-foodScoreEl.innerText = `${foodLevel}`
 
 
+// Functions
 // I want to update the score in the DOM every time I hit the button corresponding to the variable
 
 function changeFoodLevel(newFoodLevel) {
