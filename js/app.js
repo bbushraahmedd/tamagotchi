7814684e10895ec4a4aa0d1d099627751579c1bf
bbushraahmedd <bushimed98@gmail.com>
2022-11-14 08:15:12 -0600

    new files

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+
+// Welcome Page
+
+// let enterNameUrl = "entername.html";
+// document.getElementById("play").onclick = 
+// function () {
+//     window.location.replace(enterNameUrl);
+// }
+
